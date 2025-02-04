44db566cb43875b26e602a55b2a5ac7a694caf99
Daniel Libicki <daniel.libicki@granulate.io>
2025-02-04 18:25:47 +0200

    move slow and setup things

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="success_rates",
+    version="0.1.0",
+    description="A package to compute vehicle detection success rates from DuckDB-supported files.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Daniel Libicki",
+    author_email="daniel.libicki@gmail.com",
+    url="https://github.com/mccomplete/success_rates",
+    packages=find_packages(),
+    install_requires=[
+        "duckdb",
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
